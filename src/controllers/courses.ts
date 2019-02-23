34a78c2c6bf5fb94516acd76c9b415ececdccafd
unknown <jhonemillan@gmail.com>
2019-02-23 18:35:58 -0500

    fix query

--- src/controllers/courses.ts
+++ src/controllers/courses.ts
@@ -1,15 +1,27 @@
 import { BaseContext } from 'koa';
 import { Courses } from '../entity/Courses'
-import {getConnection, getRepository} from "typeorm";
+import {getConnection, getRepository, createQueryBuilder} from "typeorm";
 import { getManager, Repository, Not, Equal } from 'typeorm';
 
 export class CourseController{
-    public static async getCourses(ctx: BaseContext){
+    public static async getCourses(){
         const cRepository = getRepository(Courses);
         
         const data:Courses[]  = await cRepository.find();
+
+        return data;       
         
-        ctx.status = 200;
-        ctx.body = data;
+    }
+
+    public static async getCourse(id: number){
+        const cRepository = getRepository(Courses);
+        
+        const course  = await createQueryBuilder("Courses")
+                                      .innerJoinAndSelect("Courses.lessonss", "lessonss")
+                                      .where("Courses.id = :id",{id: id})
+                                      .getOne();
+
+        return course;       
+        
     }
 }
--- src/routes/courses.ts
+++ src/routes/courses.ts
@@ -5,8 +5,16 @@ const router = new Router({ prefix: '/course'});
 
 router
     .get('/', async(ctx)=>{
-        await CourseController.getCourses(ctx);
-    });
+        let data = await CourseController.getCourses();
+        ctx.status = 200;
+        ctx.body = data;
+    })
+
+    .get('/:id', async(ctx)=>{
+        let course = await CourseController.getCourse(ctx.params.id)
+        ctx.status = 200;
+        ctx.body = course;
+    })
 
 export { router as courserouter }
 
